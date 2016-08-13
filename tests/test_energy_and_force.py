e44ec63129b4685981d4933c3fabf266f7a15f74
Hai Nguyen <hainm.comp@gmail.com>
2016-08-12 22:51:22 -0400

    reorg code

--- tests/test_energy_and_force.py
+++ tests/test_energy_and_force.py
@@ -12,28 +12,29 @@ def test_eneregy_and_force():
     with mdgx.setup(prmtop, rst7) as context:
         mdgx_ene, mdgx_forces = context.energy_forces()
         
-        pme_input = sander.pme_input()
-        parm = pmd.load_file(prmtop, rst7)
+    pme_input = sander.pme_input()
+    parm = pmd.load_file(prmtop, rst7)
+
+    with sander.setup(prmtop, rst7, box=parm.box, mm_options=pme_input):
         
-        with sander.setup(prmtop, rst7, box=parm.box, mm_options=pme_input):
-            ene, sander_forces = sander.energy_forces()
-            sander_enegies = dict((att, getattr(ene, att)) for att in dir(ene) if not
-                    att.startswith('_'))
+        ene, sander_forces = sander.energy_forces()
+        sander_enegies = dict((att, getattr(ene, att)) for att in dir(ene) if not
+            att.startswith('_'))
         
-        print("")
-        print("potential energy")
-        print("sander_enegies")
-        print(sander_enegies['tot'])
-        print("mdgx_energies")
-        print(mdgx_ene['eptot'])
-        
-        print("")
-        print('forces')
-        print('sander_forces, first 5 atoms')
-        print(sander_forces[:15])
-        print("")
-        print('mdgx_forces, first 5 atoms')
-        print(mdgx_forces.tolist()[:15])
+    print("")
+    print("potential energy")
+    print("sander_enegies")
+    print(sander_enegies['tot'])
+    print("mdgx_energies")
+    print(mdgx_ene['eptot'])
+    
+    print("")
+    print('forces')
+    print('sander_forces, first 5 atoms')
+    print(sander_forces[:15])
+    print("")
+    print('mdgx_forces, first 5 atoms')
+    print(mdgx_forces.tolist()[:15])
 
 if __name__ == '__main__':
     test_eneregy_and_force()
