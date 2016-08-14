bc3a7a002f361851b3340f1b0c4eb9be09a5e5c4
Hai Nguyen <hainm.comp@gmail.com>
2016-08-14 18:21:15 -0400

    add assertion

--- tests/test_energy_and_force.py
+++ tests/test_energy_and_force.py
@@ -2,6 +2,7 @@ import os
 import mdgx
 import sander
 import parmed as pmd
+from numpy.testing import assert_almost_equal as aa_eq
 
 def test_eneregy_and_force():
     path = os.path.dirname(__file__) 
@@ -10,7 +11,7 @@ def test_eneregy_and_force():
     rst7 = path + '/vAla3.rst7'
     
     with mdgx.setup(prmtop, rst7) as context:
-        mdgx_ene, mdgx_forces = context.energy_forces()
+        mdgx_energies, mdgx_forces = context.energy_forces()
         
     pme_input = sander.pme_input()
     parm = pmd.load_file(prmtop, rst7)
@@ -26,7 +27,7 @@ def test_eneregy_and_force():
     print("sander_enegies")
     print(sander_enegies['tot'])
     print("mdgx_energies")
-    print(mdgx_ene['eptot'])
+    print(mdgx_energies['eptot'])
     
     print("")
     print('forces')
@@ -35,6 +36,10 @@ def test_eneregy_and_force():
     print("")
     print('mdgx_forces, first 5 atoms')
     print(mdgx_forces.tolist()[:15])
+
+    aa_eq([sander_enegies['tot'],],
+          [mdgx_energies['eptot'],],
+          decimal=4)
 
 if __name__ == '__main__':
     test_eneregy_and_force()
